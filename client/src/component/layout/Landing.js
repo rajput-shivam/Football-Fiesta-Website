80e5914931cedffb9b7328bc6d34a991a83182c3
Shivam Rajput <rajput.shivam26@gmail.com>
2021-02-18 20:59:38 +0530

    initial commit for front end

---
+++ client/src/component/layout/Landing.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from 'react-router-dom';
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Football-Fiesta</h1>
+          <p className="lead">A place where we promote football at grassroot level!!</p>
+          <p className="lead">Create your own profile, organise and participate in various amazing football compettions!!!</p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
