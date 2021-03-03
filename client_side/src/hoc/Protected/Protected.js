a0345d4ef866ce70f186ddc092f9b25fc5169df6
Shivam Rajput <rajput.shivam26@gmail.com>
2021-03-03 13:55:32 +0530

    client_side deleted

--- client_side/src/hoc/Protected/Protected.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { Redirect } from "react-router-dom";
-
-export default function Protected(props) {
-  let auth = JSON.parse(localStorage.getItem("auth"));
-  console.log(auth, "   sadjosaod ");
-
-  return <div>{auth ? <props.cmp /> : <Redirect to="login"></Redirect>}</div>;
-}
