0e45cd950de23ae3135f4ab8c7edb798aefd5125
Shivam Rajput <rajput.shivam26@gmail.com>
2021-03-22 21:46:23 +0530

    Initial Interface Completed

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Link, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+
+const Landing = ({ isAuthenticated }) => {
+  if (isAuthenticated) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">Football-Fiesta</h1>
+          <p className="lead">
+            A place where we promote football at grassroot level!<br/>
+            Create your own profile, organise and participate in various amazing
+            football compettions!!!
+          </p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+Landing.propTypes = {
+  isAuthenticated: PropTypes.bool
+};
+
+const mapStateToProps = (state) => ({
+  isAuthenticated: state.auth.isAuthenticated
+});
+
+export default connect(mapStateToProps)(Landing);
